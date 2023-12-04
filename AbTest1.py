e26705ae0a67cd67d0f6783815ecbacc48c94795
Abraham Tenorio <abraham23@vt.edu>
2023-12-03 22:54:13 -0500

    fixed code #spin to win

--- AbTest1.py
+++ AbTest1.py
@@ -1,63 +1,42 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
 import pynmea2
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import osmnx as ox
+import matplotlib.image as mpimg
+import numpy as np
 
 # Get the street network for Blacksburg, VA
-graph = ox.graph_from_address('495 Old Turner St, Blacksburg, VA 24060',dist=500,dist_type='bbox')
-#area = ox.graph_from_place('Blacksburg, Virginia, USA')
-#buildings = ox.features_from_place(query='Blacksburg, Virginia, USA', tags={'building':True})
-#north, east, south, west = buildings.total_bounds
+graph = ox.graph_from_address('495 Old Turner St, Blacksburg, VA 24060', dist=500, dist_type='bbox')
 
-#print(buildings.total_bounds)
+# Create a figure and axis
+plt.ion()
+fig, ax = ox.plot_graph(graph, show=False, close=False)
 
+# Load the icon image
 icon = mpimg.imread('icon.png')
 
-ser = "$GPGGA,202659.149,3713.668,N,08025.329,W,1,12,1.0,0.0,M,0.0,M,,*73"
-line = ser
-msg = pynmea2.parse(line)
+# Display the icon at a specific location on the map
+icon_extent = 0.0003  # Adjust icon size
+lat, lon = 37.139, -80.256  # Initial position (replace with your desired coordinates)
+icon_plot = ax.imshow(icon, extent=[lon - icon_extent, lon + icon_extent, lat - icon_extent, lat + icon_extent], zorder=10)
 
-#plt.xlim(east, west)
-#plt.ylim(north, south)
-lat, lon = msg.latitude, msg.longitude
-print(lon,lat)
-#plt.plot(lat, lon,'ro')
-# Redraw the map
-#plt.draw()
-plt.ion()
-fig, ax = ox.plot_graph(graph,show=False,close=False)
-
-me=ax.scatter(lon,lat,c='red')
-plt.draw()
-plt.pause(0.001)
-for x in range(1,100):
+for x in range(1, 100):
+    # Simulate GPS data
     ser = "$GPGGA,202659.149,3713.789,N,08025.260,W,1,12,1.0,0.0,M,0.0,M,,*71"
-    line = ser #add to be readline if needed and add if(starts with $GPGGA) before next 2 lines
-    msg = pynmea2.parse(line)
-    lat, lon = msg.latitude, msg.longitude
-    me.set_offsets([lon,lat])
-    plt.draw()
-    plt.pause(0.5)
-    ser = "$GPGGA,202659.149,3713.668,N,08025.329,W,1,12,1.0,0.0,M,0.0,M,,*73"
     line = ser
     msg = pynmea2.parse(line)
-    me.set_color('orange')
     lat, lon = msg.latitude, msg.longitude
-    me.set_offsets([lon,lat])
+
+    # Simulate heading data
+    heading_degrees = 290.85  # Replace this with your actual heading data
+
+    # Update icon position and rotation
+    icon_plot.set_extent([lon - icon_extent, lon + icon_extent, lat - icon_extent, lat + icon_extent])  # Adjust position
+
+    # Rotate the icon
+    rotated_icon = np.rot90(icon, k=int(heading_degrees / 90))
+
+    # Display the rotated icon
+    icon_plot.set_data(rotated_icon)
+
     plt.draw()
     plt.pause(0.5)
-    
-# Set the x and y limits of the map
-
-
-
-
-# In[ ]:
-
-
-
-
